8fc77c2fde50ee09ff7ec6907744f8f6e30217a6
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-18 16:59:59 -0700

    add DayListItem component with conditionals for class names

---
+++ src/components/DayListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/DayListItem.scss"
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">Day Name</h2> 
+      <h3 className="text--light">X spots remaining</h3>
+    </li>
+  );
+}
