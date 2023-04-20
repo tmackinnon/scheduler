3be7e4a0a623fa7e6240b01ddac621d1f79450c2
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-19 17:08:42 -0700

    add appointment folder to contain parent and child components. Update stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "components/Appointment/styles.scss"
+
+const Appointment = function(props) {
+
+
+  return (
+    <article className="appointment">
+    
+    </article>
+  );
+}
+
+export default Appointment;
