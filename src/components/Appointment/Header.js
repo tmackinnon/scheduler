42a31b7c2352b4d2732b2ff8e648e8ad644ce189
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-19 17:35:22 -0700

    add header and empty components and their stories

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Header = function(props) {
+  const time = props.time;
 
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
