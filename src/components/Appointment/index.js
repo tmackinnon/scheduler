87da45457cb2a131f3a1bf43f58204ed82c0468c
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-19 21:22:00 -0700

    add components to form and test with stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "components/Appointment/styles.scss"
 
 const Appointment = function(props) {
-
+  const interview = props.interview;
 
   return (
     <article className="appointment">
-    
+      <Header time={props.time}/>
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
     </article>
   );
 }
