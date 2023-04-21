c40a5ec0852d106dc97542e55ba8968d432a9467
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-20 18:50:49 -0700

    refactor setStates into obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,9 +45,14 @@ const appointments = {
 };
 
 export default function Application(props) {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("/api/days")
@@ -73,8 +78,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
       />
       </nav>
