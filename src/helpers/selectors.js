b9965bf933c6601eddb956c55ef2906bb84b444f
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-21 12:57:13 -0700

    add and test helper function to get interview data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,6 +9,19 @@ export const getAppointmentsForDay = function(state, day) {
   }
 
   const appointments = dayInfo[0].appointments;
-  
   return appointments.map(id => state.appointments[id]); //return an array of appointment objs
+}
+
+
+export const getInterview = function(state, interview) {
+  if (!interview) return null;
+  
+  const interviewerId = interview.interviewer;
+  const interviewers = state.interviewers
+  
+  for (let key in interviewers) {
+    if (interviewerId === interviewers[key].id) {
+      return {...interview, interviewer: interviewers[key]}
+    }
+  }
 }
