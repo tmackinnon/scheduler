042ffb688ab02a16b81a88ed1aeaa6be6725b19f
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-25 21:46:01 -0700

    add testing

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -75,9 +75,9 @@ const useApplicationData = () => {
 
   useEffect(() => {
     Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
+      axios.get("http://localhost:8001/api/days"), //change url to work with test 
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ])
       .then((all) => {
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
